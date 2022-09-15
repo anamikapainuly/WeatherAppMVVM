ca34990d6104be3649c450956911a1869a53a61b
AnamikaPublica <anamika@publica.co.nz>
2022-09-16 05:17:14 +1200

    no message

--- app/src/main/java/com/anupras/weatherappsample/utils/Resource.kt
+++ app/src/main/java/com/anupras/weatherappsample/utils/Resource.kt
@@ -1,5 +1,10 @@
 package com.anupras.weatherappsample.utils
 
 /**
-* Created by anamika on 16,September,2022
-*/sealed class Resource
+ * Created by anamika on 16,September,2022
+ */
+sealed class Resource<T> (val data: T? = null, val throwable: Throwable? = null){
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(throwable: Throwable, data: T? = null) : Resource<T>(data,throwable)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+}
--- app/src/main/java/com/anupras/weatherappsample/utils/networkBoundResource.kt
+++ app/src/main/java/com/anupras/weatherappsample/utils/networkBoundResource.kt
@@ -1,5 +1,32 @@
 package com.anupras.weatherappsample.utils
 
+import kotlinx.coroutines.flow.*
+
 /**
-* Created by anamika on 16,September,2022
-*/
+ * Created by anamika on 16,September,2022
+ */
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType>,
+    crossinline fetch: suspend () -> RequestType,
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,
+    crossinline shouldFetch: (ResultType) -> Boolean = { true }
+) = flow {
+    val data = query().first()      //Get List
+
+    val flow = if (shouldFetch(data)) {    //Check if data need update
+        emit(Resource.Loading(data))    // Display Progress
+
+        //If FetchResult fails , because of Internet Connection - Using try catch
+        try {
+            saveFetchResult(fetch())
+            query().map { Resource.Success(it) }
+        } catch (throwable: Throwable) {
+            query().map { Resource.Error(throwable, it) }
+        }
+    } else {
+        query().map { Resource.Success(it) }
+    }
+
+    emitAll(flow)
+}
