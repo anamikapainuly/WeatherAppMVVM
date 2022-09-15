ab7477987033b076e15c26fd36809d54dbd8c108
AnamikaPublica <anamika@publica.co.nz>
2022-09-16 05:12:34 +1200

    Adding db and DI for instance of DB and DAO

---
+++ app/src/main/java/com/anupras/weatherappsample/db/WeatherDatabase.kt
@@ -0,0 +1,14 @@
+package com.anupras.weatherappsample.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.anupras.weatherappsample.model.Data
+
+/**
+ * Created by anamika on 16,September,2022
+ */
+@Database(entities = [Data::class], version = 1)
+abstract class WeatherDatabase : RoomDatabase() {
+    abstract fun weatherDao() : WeatherDao
+
+}
--- app/src/main/java/com/anupras/weatherappsample/di/AppModule.kt
+++ app/src/main/java/com/anupras/weatherappsample/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.anupras.weatherappsample.di
 
+import android.app.Application
+import androidx.room.Room
 import com.anupras.weatherappsample.api.ApiService
+import com.anupras.weatherappsample.db.WeatherDatabase
 import com.anupras.weatherappsample.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -28,5 +33,20 @@ object AppModule {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        app: Application
+    ) = Room.databaseBuilder(app, WeatherDatabase::class.java,"weather_db")
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    fun provideWeatherDao(db: WeatherDatabase) = db.weatherDao()
+
+    @Provides
+    @Singleton
+    fun providesApplicationScope() = CoroutineScope(SupervisorJob())        //To avoid using GlobalScope
 
 }
