dc361bb45fcaf60752d1e9ea25097b98e04bdc4b
AnamikaPublica <anamika@publica.co.nz>
2022-09-16 04:41:26 +1200

    Adding Application Class and Hilt dependencies

---
+++ app/src/main/java/com/anupras/weatherappsample/WeatherApplication.kt
@@ -0,0 +1,11 @@
+package com.anupras.weatherappsample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by anamika on 16,September,2022
+ */
+@HiltAndroidApp
+class WeatherApplication: Application() {
+}
