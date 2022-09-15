abf1e448a8d607f754f89b1d33937eacaade0286
AnamikaPublica <anamika@publica.co.nz>
2022-09-16 05:08:34 +1200

    Adding Repo and viewModel

---
+++ app/src/main/java/com/anupras/weatherappsample/repository/WeatherRepo.kt
@@ -0,0 +1,7 @@
+package com.anupras.weatherappsample.repository
+
+/**
+ * Created by anamika on 16,September,2022
+ */
+class WeatherRepo {
+}
---
+++ app/src/main/java/com/anupras/weatherappsample/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,11 @@
+package com.anupras.weatherappsample.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+/**
+ * Created by anamika on 16,September,2022
+ */
+@HiltViewModel
+class WeatherViewModel : ViewModel(){
+}
